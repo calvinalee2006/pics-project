a67febe13b0b05e39f10895398fb05f71656944f
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-06-11 18:39:24 -0500

    added api

--- src/api.js
+++ src/api.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID P2GRUK1Ly-wwXqIUox6KDGCtrmPupw7XYtWvx1oW8tc",
     },
     params: {
-      query: "cars",
+      query: term,
     },
   });
 
-  console.log(response);
-
-  return response;
+  return response.data.results;
 };
 
 export default searchImages;
