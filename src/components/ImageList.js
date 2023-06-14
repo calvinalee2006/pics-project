34d61734bd14a38895ec2bf42ebd78223e73823d
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-06-14 06:13:21 -0500

    finished App. +styling & used .map to show images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,12 @@
+import ImageShow from "./ImageShow";
+import "./ImageList.css";
+
 function ImageList({ images }) {
-  return <div>ImageList: {images.length}</div>;
+  const renderedImages = images.map((image) => {
+    return <ImageShow key={image.id} image={image} />;
+  });
+
+  return <div className="image-list">{renderedImages}</div>;
 }
 
 export default ImageList;
